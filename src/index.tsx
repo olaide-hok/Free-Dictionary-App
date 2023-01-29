4ed7ee946a51abf4198e630be1139b9823e439bf
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-29 20:35:39 +0100

    Wrap App component with DictionaryProvider

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from './App'
 import {ChakraProvider} from '@chakra-ui/react'
 import theme from '../src/theme'
 import '../src/theme/styles.css'
+import {DictionaryProvider} from './context'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <DictionaryProvider>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </DictionaryProvider>
   </React.StrictMode>
 )
