a21068ab895c5c6aaea216a3a9a35d2d3bf3f8c2
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-29 20:33:40 +0100

    Remove and refactored SearchBar component

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,27 +1,51 @@
 import {
+  FormControl,
   HStack,
   Input,
   InputGroup,
   InputRightElement,
-  Spacer,
 } from '@chakra-ui/react'
+import {useEffect, useRef} from 'react'
 import {RiSearchLine} from 'react-icons/ri'
+import {useGlobalContext} from '../context'
 
 const SearchBar = () => {
+  const {setSearchWord} = useGlobalContext()
+
+  const searchWord = useRef<HTMLInputElement>(null!)
+
+  useEffect(() => {
+    if (searchWord.current) {
+      searchWord.current.focus()
+    }
+  }, [])
+
+  const searchWordFn = () => {
+    setSearchWord(searchWord.current.value)
+    console.log(searchWord.current.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLInputElement>): void => {
+    e.preventDefault()
+  }
+
   return (
     <HStack width="full" pb="12">
-      <InputGroup maxW="7xl">
-        <Input
-          variant="filled"
-          rounded="lg"
-          placeholder="Keyboard"
-          _placeholder={{fontWeight: 'bold'}}
-        />
-        <InputRightElement pointerEvents="none" children={<RiSearchLine />} />
-      </InputGroup>
-      <Spacer />
-      {/* Avatar
-      <Avatar /> */}
+      <FormControl onSubmit={handleSubmit}>
+        <InputGroup maxW="7xl">
+          <Input
+            variant="filled"
+            rounded="lg"
+            placeholder="Type here..."
+            _placeholder={{fontWeight: 'bold'}}
+            // onChange={searchWordFn}
+            onClick={searchWordFn}
+            ref={searchWord}
+            type="text"
+          />
+          <InputRightElement pointerEvents="none" children={<RiSearchLine />} />
+        </InputGroup>
+      </FormControl>
     </HStack>
   )
 }
