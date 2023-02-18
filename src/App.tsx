8c8d12167921f31d85b15eb8230ee7b60e9ffbac
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-02-18 21:54:27 +0100

    Refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import {Container, Flex} from '@chakra-ui/react'
+import {Container, Flex, useColorModeValue} from '@chakra-ui/react'
 import Header from './components/header'
 import Body from './components/body'
 
 function App() {
+  const bgColor = useColorModeValue('white', 'gray.800')
+
   return (
-    <Container maxW="container.lg" p="8">
-      <Flex direction={{md: 'column'}}>
+    <Container maxW="container.lg" p={[4, 8]} bgColor={bgColor}>
+      <Flex direction={{base: 'column'}}>
         <Header />
         <Body />
       </Flex>
